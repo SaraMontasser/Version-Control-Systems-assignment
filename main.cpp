5a929454502afa8da96053052beb4fb561d244e3
sara-montasser <saramontasser4@gmail.com>
2023-02-17 17:01:09 +0200

    print the total sum of a vector of numbers

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 
+#define vectorSize 10
 using namespace std;
 
 int getSum(vector<int> &v) {
-    int vectorSize = v.size(), totalSum = 0;
+    int totalSum = 0;
     for (int i = 0; i < vectorSize; ++i) {
         totalSum += v[i];
     }
@@ -12,6 +13,11 @@ int getSum(vector<int> &v) {
 }
 
 int main() {
-
+    vector<int> v(vectorSize);
+    for (int i = 0; i < vectorSize; i++) {
+        v[i] = i + 1;
+    }
+    int answer = getSum(v);
+    cout << "The sum is " << answer;
     return 0;
 }
