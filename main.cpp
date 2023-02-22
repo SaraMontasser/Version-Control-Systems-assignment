85dbebb8e3997022387d8cba616807405dd85ddb
sara-montasser <saramontasser4@gmail.com>
2023-02-23 01:45:07 +0200

    feat: Adding getMin function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #define vectorSize 10
 using namespace std;
@@ -12,12 +13,20 @@ int getSum(vector<int> &v) {
     return totalSum;
 }
 
+int getMin(vector<int> &v) {
+    int min_number = v.front();
+    for (int i = 1; i < vectorSize; ++i) {
+        min_number = min(min_number, v[i]);
+    }
+    return min_number;
+}
+
 int main() {
     vector<int> v(vectorSize);
     for (int i = 0; i < vectorSize; i++) {
         v[i] = i + 1;
     }
-    int answer = getSum(v);
-    cout << "The sum is " << answer;
+    int answer = getSum(v), min_number = getMin(v);
+    cout << "The sum is " << answer << endl << "The minimum number is " << min_number;
     return 0;
 }
